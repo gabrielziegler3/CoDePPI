c71b893b8b1e04bc373a500a412ce13a1d42141b
Gabriel Ziegler <gabrielziegler3@gmail.com>
2020-03-26 15:42:09 -0300

    Add K Space measurements and minimum energy functions
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- src/helpers.py
+++ src/helpers.py
@@ -2,12 +2,12 @@ import numpy as np
 from numpy.fft import fft2, fftshift
 
 
-def get_fourier_transform(img, fshift=False, use_absolute=True):
+def get_fourier_transform(img, fshift=False, use_absolute=False):
     fourier_transf = fft2(img)
 
     if fshift:
         fourier_transf = fftshift(fourier_transf)
-    magnitude_spectrum = fourier_transf
     if use_absolute:
-        magnitude_spectrum = np.log(np.abs(fourier_transf))
-    return magnitude_spectrum
+        fourier_transf = np.log(np.abs(fourier_transf))
+
+    return fourier_transf
