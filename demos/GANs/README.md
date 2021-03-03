fda4d0b220e03b5ca4c4962a60e51c245f77abec
Gabriel Ziegler <gabrielziegler3@gmail.com>
2021-03-03 13:24:11 -0300

    Minor updates
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

---
+++ demos/GANs/README.md
@@ -0,0 +1,18 @@
+# MNIST GAN
+
+## Download data 
+
+```
+```
+
+```
+cd data/
+
+wget https://github.com/myleott/mnist_png/raw/master/mnist_png.tar.gz    
+```
+
+## Extract
+
+```
+tar -xvf mnist_png.tar.gz
+```
--- jupyter.sh
+++ jupyter.sh
@@ -1,2 +1,2 @@
-jupyter notebook --ip 0.0.0.0 --port 8080 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
+jupyter notebook --ip 0.0.0.0 --port 8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
 
