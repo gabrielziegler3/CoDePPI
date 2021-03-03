149abbf7307aa501a99534eb134d2618e487f709
Gabriel Ziegler <gabrielziegler3@gmail.com>
2021-03-03 17:37:55 -0300

    Add VarNet scripts
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

---
+++ src/run_inference.sh
@@ -0,0 +1,5 @@
+python fastmri_run_pretrained_varnet_inference.py \
+	# --data_path "/mri-reconstruction/mnt/FastMRI/knee/" \
+	--data_path "/mri-reconstruction/data/FastMRI/knee/" \
+	--output_path "/mri-reconstruction/log/" \
+	--challenge "varnet_knee_mc" 
