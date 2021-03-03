ddb3dcda5f4690313f1cbefc879893014b7b174d
Gabriel Ziegler <gabrielziegler3@gmail.com>
2021-03-03 21:32:27 +0000

    Add first VarNet inference
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- src/run_inference.sh
+++ src/run_inference.sh
@@ -1,5 +1,4 @@
 python fastmri_run_pretrained_varnet_inference.py \
-	# --data_path "/mri-reconstruction/mnt/FastMRI/knee/" \
-	--data_path "/mri-reconstruction/data/FastMRI/knee/" \
+	--data_path "/mri-reconstruction/data/multicoil_test_v2/" \
 	--output_path "/mri-reconstruction/log/" \
 	--challenge "varnet_knee_mc" 
