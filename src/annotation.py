1e02f81154a277c070b81e2ff69b6744842f3f0c
Gabriel Ziegler <gabrielziegler3@gmail.com>
2021-04-22 19:04:12 -0300

    Add matrix center crop
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- src/annotation.py
+++ src/annotation.py
@@ -1,22 +1,24 @@
 import json
 import numpy as np
 import matplotlib.pylab as plt
-import cv2
+
 from skimage.draw import polygon
-
-
 from collections import defaultdict
 from typing import Union, List, Tuple
-from helpers import convert_from_ls
 
 
 class Annotation():
-    def __init__(self, filename: str):
+    def __init__(self, filename: str, crop_shape: Tuple[int, int] = None):
         self.filename = filename
+        self.crop_shape = crop_shape
         self.shape = (0, 0)
         self.annotation = defaultdict(list)
+        self.cropped_annotation = defaultdict(list)
         self.points = defaultdict(list)
         self.load_annotations()
+
+        if self.crop_shape:
+            self.crop_center(self.crop_shape)
 
     def __str__(self):
         count_values = {}
@@ -24,7 +26,6 @@ class Annotation():
             count_values[k] = len(self.annotation[k])
 
         return str(count_values)
-        # return str(self.annotation.keys())
 
     def load_annotations(self):
         with open(self.filename) as f:
@@ -37,47 +38,29 @@ class Annotation():
                 self.shape = annot["original_height"], annot["original_width"]
                 label = annot["value"]["polygonlabels"][0]
                 points = annot["value"]["points"]
-                # x = [i[0] for i in points]
-                # y = [i[1] for i in points]
                 points = self.points2pixel(points)
                 self.points[label].append(points)
                 filled_mask = self.fill_polygons(points)
                 self.annotation[label].append(filled_mask)
-                # x, y = self.convert2pixel(x, y)
-                # mask = self.create_mask(x, y)
-                # self.annotation[label] = mask
-                # self.annotation[label].append(mask)
-
-    # def convert2pixel(self, x: List, y: List) -> Tuple[int, int]:
-    #     x = [round(self.shape[0] * i / 100) for i in x]
-    #     y = [round(self.shape[1] * j / 100) for j in y]
-    #     return x, y
 
     def points2pixel(self, points):
         points = [[round(self.shape[0] * p[1] / 100),
                    round(self.shape[1] * p[0] / 100)] for p in points]
         return np.array(points, dtype=np.int32)
 
-
-    def crop_center(self):
-        """
-        Crop real image 2d numpy
-        """
+    def crop_center(self, crop_shape: Tuple[int, int]):
         y, x = self.shape
-        new_x, new_y = self.cropped_shape
+        new_x, new_y = crop_shape
         startx = x//2 - (new_x // 2)
         starty = y//2 - (new_y // 2)
-        print(self.gt_image.shape)
-        cropped_img = self.gt_image[starty:starty+new_y, startx:startx+new_x]
-        print(cropped_img.shape)
-        cropped_tensor = T.to_tensor(cropped_img)
-        self.cropped_kspace = fastmri.fft2c(cropped_tensor)
+        cropped_annotation = defaultdict(list)
 
-    # def fill_polygons(self, points: List[List]):
-    #     print(points)
-    #     mask = np.zeros(shape=self.shape, dtype=np.uint8)
-    #     cv2.fillPoly(mask, pts=points, color=255)
-    #     return mask
+        for k, v in self.annotation.items():
+            for idx, label_annot in enumerate(self.annotation[k]):
+                cropped_annotation[k].append(label_annot[starty:starty+new_y, startx:startx+new_x])
+
+        self.shape = crop_shape
+        self.annotation = cropped_annotation
 
     def fill_polygons(self, points: List[List]):
         mask = np.zeros(shape=self.shape, dtype=np.uint8)
@@ -94,5 +77,12 @@ class Annotation():
         return self.annotation[label]
 
     def disp_matrix(self, label: str, idx=0):
-        plt.title(label)
-        plt.imshow(self.annotation[label][idx], cmap='gray')
+        if label == "all":
+            masks = np.zeros(shape=self.shape, dtype=np.uint8)
+            for annot in list(self.annotation.values()):
+                for label_annot in annot:
+                    masks += label_annot
+            plt.imshow(masks, cmap='gray')
+            plt.colorbar()
+        else:
+            plt.imshow(self.annotation[label][idx], cmap='gray')
